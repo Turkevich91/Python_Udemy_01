fab4159a2a43d397a567df3a05ce4f37eca4c5a7
Vitalii Turkevych <vetalturkevich@gmail.com>
2019-12-27 14:13:30 -0500

    refactor

--- randomNumbers.py
+++ randomNumbers.py
@@ -10,7 +10,6 @@ def drop():
 while bool(x) is not True:
     print("") if x is None else print("None")
     print("Random numbers is: ", drop(), "and", drop())
-    # print(f"Random numbers is: {random.randint(1, 6)} and {random.randint(1, 6)}")
     print("Input text or keep empty: ")
     x = input()
 
